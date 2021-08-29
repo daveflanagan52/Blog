75d5ab1917e94d4b55788a411f4be1689150fb3b
Dave <dave.flanagan52@gmail.com>
2021-08-29 17:25:39 +0300

    Add photos to about page

--- packages/client/src/Views/About/index.tsx
+++ packages/client/src/Views/About/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
+import dave from '../../Resources/dave.jpg';
+import suvi from '../../Resources/suvi.jpg';
+
 import Container from '../../Components/Container';
 import Location from '../../Components/Location';
 import RandomPosts from '../../Components/RandomPosts';
+import Row from '../../Components/Row';
+import Column from '../../Components/Column';
 
 const About: React.FC = () => (
   <>
@@ -13,13 +18,29 @@ const About: React.FC = () => (
     <Container>
       <section className="about">
         <h1 className="title">Dave</h1>
+        <Row>
+          <Column md={3}>
+            <img src={dave} className="img-fluid" />
+          </Column>
+          <Column md={9}>
+            
+          </Column>
+        </Row>
       </section>
 
       <section className="about">
         <h1 className="title">Suvi</h1>
+        <Row>
+          <Column md={9}>
+
+          </Column>
+          <Column md={3}>
+            <img src={suvi} className="img-fluid" />
+          </Column>
+        </Row>
       </section>
 
-      <section className="about">
+      <section className="about clearfix">
         <h1 className="title">George</h1>
       </section>
 
